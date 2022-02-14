173348f1def577c6cfc23be3ad98796ab7a01fe5
Isaac <mercado.isaac619@gmail.com>
2022-02-13 18:07:01 -0800

    readme file created

---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+# alksdj
+  
+## Description 
+
+alksdjf\
+
+## Table of Contents
+
+*[Installation](#Installation)
+*[Usage](#Usage-information)
+*[Contributing](#Contribuitng-guidelines)
+*[Tests](#Tests)
+*[Questions](#Questions)
+
+## Installation
+
+laksdjflk
+
+## Usage information
+
+dlkfjaklsdjf
+
+## Contributing guidelines
+
+laksdjflkajsd\
+
+## Tests
+
+asldkfjalksdjf
+
+## Questions
+
+dslkfjalksdj
--- index.js
+++ index.js
@@ -1,67 +1,89 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const markDown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of the project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide description of the project.'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Installation guidelines.'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Usage information.'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What licenses do you have?',
-        choices: ['MIT','UCSD','SDSU','UCBerkley']
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Contributing guidelines.'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Test instructions.'
-    },
-    {
-        type: 'input',
-        name: 'gitHub',
-        message: 'Please input your GitHub URL.'
-    },
-];
+const generateMarkdown = ({title, description, installation, usage, contributing, tests, gitHub}) =>
+`# ${title}
+  
+## Description 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+${description}
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((inquirerResponse, data) => {
-        console.log('Making ReadMe');
-        fs.writeFileSync('ReadMe.md', inquirerResponse, data);
+## Table of Contents
+
+*[Installation](#Installation)
+*[Usage](#Usage-information)
+*[Contributing](#Contribuitng-guidelines)
+*[Tests](#Tests)
+*[Questions](#Questions)
+
+## Installation
+
+${installation}
+
+## Usage information
+
+${usage}
+
+## Contributing guidelines
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Questions
+
+${gitHub}`;
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of the project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide description of the project.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Installation guidelines.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Usage information.'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What licenses do you have?',
+            choices: ['MIT','UCSD','SDSU','UCBerkley']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Contributing guidelines.'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Test instructions.'
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'Please input your GitHub URL.'
+        }    
+    ])
+    .then((answers) => {
+        const readMePage = generateMarkdown(answers);
+        console.log('Made ReadMe.md!')  
+        fs.writeFileSync('ReadMe.md', readMePage, (err) =>
+            err ? console.log(err) : console.log('Successfully created ReadMe.md!'))
     })
-    .catch((err) => {
-        console.log(err);
-    })
-}
-
-// Function call to initialize app
-init();
